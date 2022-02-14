a8f1702252baa8fdaefff18c9797f7e6ebec491a
Arno Debelle <debelle.a@gmail.com>
2022-02-14 15:33:44 +0100

    writing tests

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,20 +5,20 @@ class QuestionsController < ApplicationController
   def answer
     @question = params[:question]
     @message_display =
-      # if @question == 'I am going to work'
-      #   p 'Great!'
-      # elsif @question.include?('?')
-      #   p 'Silly question, get dressed and got to work!'
-      # else
-      #   p "I don't care, get dressed and go to work!"
-      # end
-      case @question
-      when 'I am going to work'
-        'Great!'
-      when @question.include?('?')
-        'Silly question, get dressed and go to work!'
+      if @question == 'I am going to work'
+        p 'Great!'
+      elsif @question.include?('?')
+        p 'Silly question, get dressed and go to work!'
       else
-        "I don't care, get dressed and go to work!"
+        p "I don't care, get dressed and go to work!"
       end
+    # case @question
+    # when @question.include?('?')
+    #   'Silly question, get dressed and go to work!'
+    # when 'I am going to work'
+    #   'Great!'
+    # else
+    #   "I don't care, get dressed and go to work!"
+    # end
   end
 end
